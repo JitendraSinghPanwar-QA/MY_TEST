284534805c9089c68516c1399152dbf4c696ecd9
JitendraPanwar <JitendraPanwar@RC-IND-D46YBS3>
2024-09-19 09:47:26 +0530

    My First commit

---
+++ src/test/java/Task_1/MyFirstProgram.java
@@ -0,0 +1,14 @@
+package Task_1;
+
+public class MyFirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+	System.out.println("My First Hello World- Java Program");
+		
+	}
+
+}
+
